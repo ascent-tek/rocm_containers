e6bc0763caa26d38cc9682fcd712a7607e1caa0a
Peter Weir <peterjweir@gmail.com>
2023-09-19 11:02:25 -0700

    added performance improvement command for amd card to runDorckerfile.sh script

--- amdtransformers/runDockerfile.sh
+++ amdtransformers/runDockerfile.sh
@@ -1,1 +1,2 @@
+sudo rocm-smi --setperflevel high
 docker run --rm --mount  src=/media/atek/T7/hfcache,target=/home/workspace/huggingface,type=bind --device /dev/kfd --device /dev/dri/renderD128 amdrocm_transformers_at:1
