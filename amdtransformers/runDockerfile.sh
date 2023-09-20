17a08891bf102f48ec41f0ac397ba7306c1a5e78
Peter Weir <peterjweir@gmail.com>
2023-09-20 11:14:18 -0700

    added 30b nvme offload example, changes batch size to 20, resolves a directory fault, and moves mount points to enable nvme offload

--- amdtransformers/runDockerfile.sh
+++ amdtransformers/runDockerfile.sh
@@ -1,2 +1,2 @@
 sudo rocm-smi --setperflevel high
-docker run --rm --mount  src=/media/atek/T7/hfcache,target=/home/workspace/huggingface,type=bind --device /dev/kfd --device /dev/dri/renderD128 amdrocm_transformers_at:1
+docker run --rm --mount  src=/mnt/fast/hfcache,target=/home/workspace/huggingface,type=bind --device /dev/kfd --device /dev/dri/renderD128 amdrocm_transformers_at:1
